03766fe2eafa211e10c64a603854a68ce5d1f366
carreivan <carreaguileraivan@gmail.com>
2023-10-03 19:24:13 -0300

    first commit

---
+++ back-end/app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $usuarios = Usuario::all();
+        return response()->json($usuarios);
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'usuario' => 'required|unique:usuarios,usuario',
+            'clave' => 'required',
+            'habilitado' => 'boolean',
+            'idrol' => 'required|exists:roles,idrol',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $usuario = Usuario::create($request->all());
+
+        return response()->json($usuario, 201);
+    }
+
+
+    public function show($id)
+    {
+        $usuario = Usuario::find($id);
+
+        if (!$usuario) {
+            return response()->json(['error' => 'Usuario no encontrado'], 404);
+        }
+
+        return response()->json($usuario);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+
+            'usuario' => 'unique:usuarios,usuario,' . $id,
+
+            'habilitado' => 'boolean',
+            'idrol' => 'exists:roles,idrol',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $usuario = Usuario::findOrFail($id);
+        $usuario->update($request->all());
+
+        return response()->json($usuario, 200);
+    }
+
+    public function register(Request $request)
+    {
+
+        $validator = Validator::make($request->all(), [
+
+            'usuario' => 'unique:usuarios,usuario',
+            'clave' => 'required',
+            'habilitado' => 'boolean',
+            'idrol' => 'exists:roles,idrol',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+
+        $usuario = new Usuario();
+        $usuario->usuario = $request->input('usuario');
+        $usuario->clave = $request->input('clave');
+        $usuario->habilitado = $request->input('habilitado');
+        $usuario->idrol = $request->input('idrol');
+        $usuario->save();
+
+
+        return response()->json(['message' => 'Usuario registrado con éxito', 'usuario' => $usuario], 201);
+    }
+
+    public function login(Request $request)
+    {
+
+        $auth = Usuario::where('usuario', $request->usuario)->where('clave', $request->clave)->first();
+
+        if (!$auth) {
+            return response()->json(['error' => 'Credenciales incorrectas'], 401);
+        }
+
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Inicio de sesión exitoso',
+            'token' => 'el-token-de-autenticación',
+            'idrol' => $auth->idrol,
+            'usuario' => $auth,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $usuario = Usuario::findOrFail($id);
+        $usuario->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ front-end/src/App.jsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginPage from "./components/LoginPage";
+import RegisterPage from "./components/RegisterPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
